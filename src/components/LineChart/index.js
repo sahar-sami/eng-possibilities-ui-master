04d98b50f46c6f09b1fd433f0b028d865e3db0a4
chloegoh <chloegoh@umich.edu>
2021-02-26 00:12:46 +0800

    Add backend integration

--- src/components/LineChart/index.js
+++ src/components/LineChart/index.js
@@ -2,86 +2,75 @@ import React from "react";
 import { Line } from "react-chartjs-2";
 import "./style.css";
 
-const data = {
-  labels: [
-    "2021",
-    "2022",
-    "2023",
-    "2024",
-    "2025",
-    "2026",
-    "2027",
-    "2028",
-    "2029",
-    "2030",
-  ],
-  datasets: [
-    {
-      label: "Investment Growth",
-      data: [
-        10000.0,
-        10050.3,
-        12125.35,
-        12089.99,
-        13009.2,
-        13000.0,
-        14850.6,
-        17098.245,
-        17980.4353,
-        19871.0,
-      ],
-      fill: false,
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgba(255, 99, 132, 0.2)",
-      // to draw a straight line
-      lineTension: 0,
-    },
-  ],
-};
-
-const options = {
-  maintainAspectRatio: false,
-  animation: { duration: 2000 },
-  scales: {
-    yAxes: [
+const LineChart = (props) => {
+  const data = {
+    labels: [
+      "2021",
+      "2022",
+      "2023",
+      "2024",
+      "2025",
+      "2026",
+      "2027",
+      "2028",
+      "2029",
+      "2030",
+    ],
+    datasets: [
       {
-        scaleLabel: {
-          display: false,
-          labelString: "Amount",
-        },
-        ticks: {
-          // Include a dollar sign in the ticks
-          callback: function (value, index, values) {
-            return "$" + value;
+        label: "Investment Growth",
+        data: props.investmentGrowth,
+        fill: false,
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgba(255, 99, 132, 0.2)",
+        // to draw a straight line
+        lineTension: 0,
+      },
+    ],
+  };
+
+  const options = {
+    maintainAspectRatio: false,
+    animation: { duration: 2000 },
+    scales: {
+      yAxes: [
+        {
+          scaleLabel: {
+            display: false,
+            labelString: "Amount",
+          },
+          ticks: {
+            // Include a dollar sign in the ticks
+            callback: function (value, index, values) {
+              return "$" + value;
+            },
           },
         },
-      },
-    ],
-    xAxes: [
-      {
-        scaleLabel: {
-          display: true,
-          labelString: "Year",
+      ],
+      xAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: "Year",
+          },
+        },
+      ],
+    },
+    tooltips: {
+      callbacks: {
+        label: function (tooltipItem, data) {
+          let label = "Amount";
+          if (label) {
+            label += ": $";
+          }
+          // rounds value to 2 decimal places
+          label += Math.round(tooltipItem.yLabel * 100) / 100;
+          return label;
         },
       },
-    ],
-  },
-  tooltips: {
-    callbacks: {
-      label: function (tooltipItem, data) {
-        let label = "Amount";
-        if (label) {
-          label += ": $";
-        }
-        // rounds value to 2 decimal places
-        label += Math.round(tooltipItem.yLabel * 100) / 100;
-        return label;
-      },
     },
-  },
-};
+  };
 
-const LineChart = () => {
   return (
     <div className="chart-div">
       <Line data={data} options={options} />
--- src/pages/Forecaster/index.js
+++ src/pages/Forecaster/index.js
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 import LineChart from "../../components/LineChart";
 import InputTable from "../../components/Table/Table.js";
 
 const ForecasterHome = () => {
+  const [categoryData, setCategoryData] = useState([]);
+  const [investmentGrowth, setInvestmentGrowth] = useState([]);
+
+  useEffect(() => {
+    // fetch investment categories data after first render
+    async function fetchData() {
+      const response = await fetch("http://localhost:8080/api/v1/forecast", {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": "http://localhost:3000",
+        },
+      });
+
+      const data = await response.json();
+
+      setCategoryData(data);
+    }
+
+    fetchData();
+  }, []);
+
+  const checkAllocations = () => {
+    // let allocations = {"Energy": 15.0, "Technology": 50.4};
+    // check whether all add up to 100%
+    // true, get calculated investment growth to update graph
+    /*async function updateAllocations() {
+      const response = await fetch("http://localhost:8080/api/v1/forecast", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json;charset=UTF-8",
+          "Access-Control-Allow-Origin": "http://localhost:3000",
+          "body": JSON.stringify(allocations)
+        },
+      });
+
+      const data = await response.json();
+
+      setInvestmentGrowth(data);
+    }
+
+    updateAllocations();*/
+    // false, display error
+  };
+
   return (
     <>
       <div>
@@ -11,7 +57,21 @@ const ForecasterHome = () => {
           This page allows you to customize your investments and view the
           potential growth of <b>$10,000</b> over a period of <b>10 years.</b>
         </p>
-        <LineChart />
+
+        <LineChart
+          investmentGrowth={[
+            10000.0,
+            10050.3,
+            12125.35,
+            12089.99,
+            13009.2,
+            13000.0,
+            14850.6,
+            17098.245,
+            17980.4353,
+            19871.0,
+          ]}
+        />
 
         <InputTable
           entries={[
@@ -20,6 +80,14 @@ const ForecasterHome = () => {
             { category: "Financial Services", max: 4, allocation: 0 },
           ]}
         />
+
+        <Button
+          variant="outline-dark"
+          style={{ float: "right" }}
+          // onClick={checkAllocations}
+        >
+          Update
+        </Button>
       </div>
     </>
   );
